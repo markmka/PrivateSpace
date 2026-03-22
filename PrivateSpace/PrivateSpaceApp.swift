398a71fae4751d87c02e4de1d49aba61af4a6465
markmka <mka431132@gmail.com>
2026-03-22 10:12:03 +0800

    Initial Commit

---
+++ PrivateSpace/PrivateSpaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrivateSpaceApp.swift
+//  PrivateSpace
+//
+//  Created by bot on 2026/3/22.
+//
+
+import SwiftUI
+
+@main
+struct PrivateSpaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
